eb095dbce22d183798f2fee1b4b3f0f9ce0a609b
eavehh <suleimanovv73@icloud.com>
2025-01-02 16:36:54 +0300

    Add files via upload

---
+++ main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+    "fmt"
+    "os"
+    "net/http"
+//    "time"
+)
+
+ //обработчик HTTP-запросов
+ /*
+    w http.ResponseWriter — интерфейс для записи ответа на HTTP-запрос.
+    r *http.Request — указатель на структуру запроса, содержащую информацию о HTTP-запросе, который был сделан.
+*/
+
+/*
+
+func myHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Serving %s\n", r.URL.Path)
+    fmt.Printf("Served: %s\n", r.Host)
+}
+
+/*
+    Это еще одна функция-обработчик, называемая timeHandler, которая возвра-
+    щает текущее время в формате HTML. Все вызовы fmt.Fprintf() отправляют
+    данные обратно HTTP-клиенту, тогда как выходные данные fmt.Printf() вы-
+    водятся на терминале, на котором работает веб-сервер.
+*/
+
+/*
+func timeHandler(w http.ResponseWriter, r *http.Request) {
+	t := time.Now().Format(time.RFC1123)
+	Body := "The current time is:"
+	fmt.Fprintf(w, "<h1 align=\"center\">%s</h1>", Body)
+	fmt.Fprintf(w, "<h2 align=\"center\">%s</h2>\n", t)
+
+	fmt.Fprintf(w, "Serving: %s\n", r.URL.Path)
+	fmt.Printf("Served time for: %s\n", r.Host)
+}
+*/
+
+func myFunc(w http.ResponseWriter, r *http.Request){
+    fmt.Fprintf(w, "hello, Im server")
+    fmt.Println("Im a client")
+}
+
+
+func main() {
+    PORT := ":8001"
+    arguments := os.Args
+
+    if len(arguments) != 1{
+        PORT = arguments[1]
+    }
+    fmt.Println("Using port number", PORT)
+
+//    http.HandleFunc("/time", timeHandler)
+//    http.HandleFunc("/", myHandler)
+    http.HandleFunc("/my", myFunc)
+
+    err := http.ListenAndServe(PORT, nil)
+    if err != nil{
+        fmt.Println(err)
+        return
+    }
+}
